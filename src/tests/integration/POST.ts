73b137b4ac6672732a7499ee2a2a2d41320c7e4d
Galla <gallant.thomas@outlook.com>
2023-02-25 23:10:04 -0500

    Making progress on POST request tests.

--- src/tests/integration/POST.ts
+++ src/tests/integration/POST.ts
@@ -1,12 +1,10 @@
 let request = require('supertest');
 require('dotenv').config();
+import {ErrorMessage400, ErrorMessage401} from "./testData";
 
 const preRequest = request('https://' + process.env.AUTH0_BASE_URL);
-
 request = request('http://localhost:3001');
-
 let token = null;
-
 
 before(function (done) {
     preRequest
@@ -24,24 +22,64 @@ before(function (done) {
         });
 });
 
-describe('Test 400 POST errors', function () {
-
-    it('Post empty body returns 400 error message', function (done) {
-        request
-            .post('/api/v1/user/activeGames')
-            .send()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', 'Bearer ' + token)
-            .expect('Content-Type', /json/)
-            .expect(400, {
-                Error_Message: 'Invalid Request',
-                Status: 400
-            })
-            .end(function(err, res) {
-                if (err) return done(err);
-                return done();
-            });
+describe('Test POST requests for /api/v1/user/activeGames', function () {
+    describe('Test code 400 POST requests', function () {
+        it('Post empty body returns 400 error message', function (done) {
+            request
+                .post('/api/v1/user/activeGames')
+                .send()
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .expect('Content-Type', /json/)
+                .expect(400, ErrorMessage400)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    return done();
+                });
+        });
     });
 
+    describe('Test code 401 POST requests', function () {
+        it('Post no Auth header returns 401 error message', function (done) {
+            request
+                .post('/api/v1/user/activeGames')
+                .send()
+                .set('Content-Type', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(401, ErrorMessage401)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    return done();
+                });
+        });
 
+        it('Post invalid Auth header returns 401 error message', function (done) {
+            request
+                .post('/api/v1/user/activeGames')
+                .send()
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer')
+                .expect('Content-Type', /json/)
+                .expect(401, ErrorMessage401)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    return done();
+                });
+        });
+
+        // todo
+        it('Post expired Auth header returns 401 error message', function (done) {
+            request
+                .post('/api/v1/user/activeGames')
+                .send()
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer')
+                .expect('Content-Type', /json/)
+                .expect(401, ErrorMessage401)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    return done();
+                });
+        });
+    });
 });
---
+++ src/tests/integration/testData.ts
@@ -0,0 +1,16 @@
+
+export const ErrorMessage400 = {
+    Error_Message: 'Invalid Request',
+    Status: 400
+};
+
+export const ErrorMessage401 = {
+    Error_Message: 'Invalid Permission',
+    Status: 401
+};
+
+export const ErrorMessage500 = {
+    Error_Message: 'Invalid Permission',
+    Status: 401
+};
+
