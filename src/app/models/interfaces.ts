709fa7e11533d6a87591069a1daa9d87ecac426e
Galla <gallant.thomas@outlook.com>
2023-03-01 14:04:40 -0500

    removing old interfaces

--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -41,31 +41,6 @@ interface numWrongCellsPlayedPerStrategy {
     SINGLES_CHAINING: number
 }
 
-interface strategyTypes {
-    NAKED_SINGLE: boolean,
-    HIDDEN_SINGLE: boolean,
-    NAKED_PAIR: boolean,
-    NAKED_TRIPLET: boolean,
-    NAKED_QUADRUPLET: boolean,
-    NAKED_QUINTUPLET: boolean,
-    NAKED_SEXTUPLET: boolean,
-    NAKED_SEPTUPLET: boolean,
-    NAKED_OCTUPLET: boolean,
-    HIDDEN_PAIR: boolean,
-    HIDDEN_TRIPLET: boolean,
-    HIDDEN_QUADRUPLET: boolean,
-    HIDDEN_QUINTUPLET: boolean,
-    HIDDEN_SEXTUPLET: boolean,
-    HIDDEN_SEPTUPLET: boolean,
-    HIDDEN_OCTUPLET: boolean,
-    POINTING_PAIR: boolean,
-    POINTING_TRIPLET: boolean,
-    BOX_LINE_REDUCTION: boolean,
-    X_WING: boolean,
-    SWORDFISH: boolean,
-    SINGLES_CHAINING: boolean
-}
-
 interface moves {
     puzzleCurrentState: string,
     puzzleCurrentNotesState: string
@@ -79,51 +54,6 @@ interface moves {
  * //todo make casing of types consistant
  *
  */
-export interface Puzzle {
-    puzzle: string,
-    puzzleSolution: string,
-    strategies: strategyTypes,
-    difficulty: number,
-    fastestSolveTime: number,
-    averageSolveTime: number,
-    numUsersPlayed: number,
-    numTimesPlayed: number,
-    trulyUnique: boolean,
-    drillStrategies: strategyTypes,
-    calendarDate?: Date,
-    imageUrl?: string,
-    description?: string
-}
-
-export interface UserProfile {
-    userId: string,
-    userEmail: string,
-    userName: string,
-    userPreferences: {
-        savePuzzleData: boolean,
-        theme: string,
-        gamePreferences: {
-            notifyOnWrongCell: boolean,
-            highlightAllSelectedNumber: boolean,
-            highlightSelectedBox: boolean,
-            highlightSelectedRow: boolean,
-            playMusic: boolean,
-            musicIntensify: boolean
-        }
-    }
-}
-
-export interface UserGameSearchFilters {
-    userId: string,
-    gameSearchPreferences: {
-        defaultSearchType: string,
-        difficulty: {
-            low: number,
-            high: number
-        },
-        strategyTypes: strategyTypes
-    }
-}
 
 export interface userActiveGames {
     userID: string,
@@ -132,30 +62,5 @@ export interface userActiveGames {
     moves: moves[],
     numHintsAskedFor: number,
     numWrongCellsPlayed: number,
-    numWrongCellsPlayedPerStrategy: numWrongCellsPlayedPerStrategy
-}
-
-export interface userGameStats {
-    userID: string,
-    dateRange: string,
-    gamesPlayed: [{
-        puzzle: string,
-        moves: moves[],
-        numTimesPlayed: number,
-        initialSolveTime: number,
-        fastestSolveTime: number,
-        averageMoveTime: number,
-        numHintsAskedFor: number,
-        numWrongCellsPlayed: number,
-        numCorrectCellsPlayed: number,
-        numWrongCellsPlayedPerStrategy: numWrongCellsPlayedPerStrategy
-    }],
-    averageSolveTime: number,
-    fastestSolveTime: number,
-    averageMoveTime: number,
-    numHintsAskedFor: number,
-    numWrongCellsPlayed: number,
-    numGamesPlayed: number,
-    numGamedFailed: number,
     numWrongCellsPlayedPerStrategy: numWrongCellsPlayedPerStrategy
 }
